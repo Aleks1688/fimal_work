28ebccb8b2e32757cf3931e741758e65706eb6f6
Sasha <sfh1688@qq.com>
2024-07-03 22:46:40 +0800

    added files

---
+++ final_work.cs
@@ -0,0 +1,33 @@
+﻿
+string[] oldArr = new string[5] {"123", "23", "hello", "world", "res"}; //исходный массив
+//string[] oldArr = new string[4] {"1234", "1567", "-2", "computer science"};
+//string[] oldArr = new string[3] {"Russia", "Denmark", "Kazan"};
+
+string[] newArr = new string[oldArr.Length]; //массив, который принимает результат
+
+string[] RemoveLongString(string[] array1, string[] array2) //функция, которая убирает короткие строки
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+    return array2;
+}
+
+void PrintArray(string[] array)  //функция, которая печатает массив
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+RemoveLongString(oldArr, newArr); //вызываем фукцию, которая убирает длинные строки
+PrintArray(oldArr); //печать исходного массива
+PrintArray(newArr); //печать нового массива 
